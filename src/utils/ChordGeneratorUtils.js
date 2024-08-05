146714cd3e07626852aedd4b2b046dff3133cdd1
shai chen <79250138+shaichen@users.noreply.github.com>
2024-08-05 09:10:09 +0300

    added chord Modes

--- src/utils/ChordGeneratorUtils.js
+++ src/utils/ChordGeneratorUtils.js
@@ -53,6 +53,27 @@ const generateExtended = () => {
   
   return seventh.map(note => valueToNote[note] + '4');
 };
+const generateJazzChords = () => {
+  const seventh = generateSeventh().map(note => noteValues[note.slice(0, -1)]);
+  let isDominant=false;
+  //check if chord contains major third and minor srevnth
+  if (seventh[1] - seventh[0] === 4 && seventh[3] - seventh[0] === 10) {
+    isDominant = true;
+  } else {
+    isDominant = false;
+  }
+  //omit the fifth if it is a dominant chord
+  if (isDominant) {
+    seventh.splice(2, 1);
+  }
+  const extensions = [14, 17, 21]; // 9th, 11th, 13th
+  const numExtensions = Math.floor(Math.random() * 2) + 1; // 1 or 2 extensions
+  for (let i = 0; i < numExtensions; i++) {
+    const extension = extensions[Math.floor(Math.random() * extensions.length)];
+    seventh.push((seventh[0] + extension) % 12);
+  }
+  return seventh.map(note => valueToNote[note] + '4');
+};
 
 export function generateCompleteChord(questionMode, numNotes = 4) {
   let chord;

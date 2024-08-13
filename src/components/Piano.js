0f0762831a07c1fe5eb7ea5e2440d16fbe2bca97
shai chen <79250138+shaichen@users.noreply.github.com>
2024-08-13 11:22:03 +0300

    2 octave piano

--- src/components/Piano.js
+++ src/components/Piano.js
@@ -2,7 +2,7 @@ import React, { useEffect, useRef } from 'react';
 import '../styles/Piano.css';
 
 const Piano = ({ feedback, gameState, currentQuestion, onGuess, pianoSound, playNote, showCheckmark }) => {
-    const notes = ['C4', 'Db4', 'D4', 'Eb4', 'E4', 'F4', 'Gb4', 'G4', 'Ab4', 'A4', 'Bb4', 'B4'];
+    const notes = ['C4', 'Db4', 'D4', 'Eb4', 'E4', 'F4', 'Gb4', 'G4', 'Ab4', 'A4', 'Bb4', 'B4', 'C5', 'Db5', 'D5', 'Eb5', 'E5', 'F5', 'Gb5', 'G5', 'Ab5', 'A5', 'Bb5', 'B5'];
     const pianoRef = useRef(null);
 
     useEffect(() => {
@@ -28,7 +28,7 @@ const Piano = ({ feedback, gameState, currentQuestion, onGuess, pianoSound, play
             <div className="piano" ref={pianoRef}>
                 {notes.map((note, index) => (
                     <div
-                        key={note}
+                        key={index}
                         className={`key ${note.includes('b') ? 'black' : 'white'}`}
                         onClick={() => handleKeyClick(note)}
                     >

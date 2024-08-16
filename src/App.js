6d3b2c42f2ec2f2a282cdbe216791159be827a1d
shai chen <79250138+shaichen@users.noreply.github.com>
2024-08-16 11:18:32 +0300

    deployed works

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
-//test1
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { PianoProvider } from './PianoContext';
 import Navbar from './components/NavBar';
 import LandingPage from './components/LandingPage';
@@ -14,6 +12,7 @@ import Triads from './components/LearningPages/Triads';
 import SeventhChord from './components/LearningPages/SeventhChord';
 import Tensions from './components/LearningPages/Tensions';
 import './styles/App.css';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   const [pianoSound, setPianoSound] = useState(true);
@@ -24,33 +23,31 @@ function App() {
   const closeDocumentation = () => setIsDocumentationOpen(false);
 
   return (
-    <Router>
-      <PianoProvider>
-        <div className="App">
-          <Navbar
-            openDocumentation={openDocumentation}
-            pianoSound={pianoSound}
-            setPianoSound={setPianoSound}
-            gameLength={gameLength}
-            setGameLength={setGameLength}
-          />
-          <main className="main-content">
-            <Routes>
-              <Route path="/" element={<LandingPage />} />
-              <Route path="/learn" element={<Learn />} />
-              <Route path="/learn/MajorScales" element={<MajorScales />} />
-              <Route path="/learn/Triads" element={<Triads />} />
-              <Route path="/learn/SeventhChord" element={<SeventhChord />} />
-              <Route path="/learn/Tensions" element={<Tensions />} />
-              <Route path="/practice" element={<Practice />} />
-              <Route path="/quiz" element={<Quiz />} />
-              <Route path="/play/:section/:mode" element={<AppContent pianoSound={pianoSound} gameLength={gameLength} />} />
-            </Routes>
-          </main>
-          <Documentation isOpen={isDocumentationOpen} onClose={closeDocumentation} />
-        </div>
-      </PianoProvider>
-    </Router>
+    <PianoProvider>
+      <div className="App">
+        <Navbar
+          openDocumentation={openDocumentation}
+          pianoSound={pianoSound}
+          setPianoSound={setPianoSound}
+          gameLength={gameLength}
+          setGameLength={setGameLength}
+        />
+        <main className="main-content">
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/learn" element={<Learn />} />
+            <Route path="/learn/MajorScales" element={<MajorScales />} />
+            <Route path="/learn/Triads" element={<Triads />} />
+            <Route path="/learn/SeventhChord" element={<SeventhChord />} />
+            <Route path="/learn/Tensions" element={<Tensions />} />
+            <Route path="/practice" element={<Practice />} />
+            <Route path="/quiz" element={<Quiz />} />
+            <Route path="/play/:section/:mode" element={<AppContent pianoSound={pianoSound} gameLength={gameLength} />} />
+          </Routes>
+        </main>
+        <Documentation isOpen={isDocumentationOpen} onClose={closeDocumentation} />
+      </div>
+    </PianoProvider>
   );
 }
 
--- src/firebase.js
+++ src/firebase.js
@@ -2,16 +2,16 @@ import { initializeApp } from 'firebase/app';
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
-  measurementId: "YOUR_MEASUREMENT_ID"
+  apiKey: "YOUR_VALID_API_KEY",
+  authDomain: "chord-spelling-master.firebaseapp.com",
+  projectId: "chord-spelling-master",
+  storageBucket: "chord-spelling-master.appspot.com",
+  messagingSenderId: "YOUR_VALID_MESSAGING_SENDER_ID",
+  appId: "YOUR_VALID_APP_ID",
+  measurementId: "YOUR_VALID_MEASUREMENT_ID"
 };
 
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
-export { analytics };
+export { app, analytics };

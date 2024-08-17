4a703999cda45e7827053923969d8cb6afbe0d20
shai chen <79250138+shaichen@users.noreply.github.com>
2024-08-17 12:43:00 +0300

    audio better

--- src/PianoContext.js
+++ src/PianoContext.js
@@ -8,7 +8,7 @@ export const usePiano = () => useContext(PianoContext);
 
 export const PianoProvider = ({ children }) => {
     const [sampler, setSampler] = useState(null);
-    const notes = Object.keys(pianoSounds).filter(note => note !== 'C1'); // Exclude C1 which is a click sound
+    const notes = Object.keys(pianoSounds).filter(note => note !== 'C1');
 
     useEffect(() => {
         const samplerUrls = {};
@@ -16,33 +16,48 @@ export const PianoProvider = ({ children }) => {
             samplerUrls[note] = pianoSounds[note];
         });
 
-        const volume = new Tone.Volume(-12).toDestination(); // -6dB is approximately half volume
+        const volume = new Tone.Volume(-12).toDestination();
+        const limiter = new Tone.Limiter(-3).connect(volume);
 
         const newSampler = new Tone.Sampler({
             urls: samplerUrls,
             baseUrl: "",
+            attack: 0.005,
+            release: 0.1,
+            curve: 'exponential',
             onload: () => {
                 setSampler(newSampler);
             }
-        }).connect(volume); // Connect the sampler to the volume node
+        }).connect(limiter);
 
         return () => {
             if (newSampler) {
                 newSampler.dispose();
             }
             volume.dispose();
+            limiter.dispose();
         };
     }, []);
 
     const playNote = (note, duration = '8n') => {
         if (sampler) {
-            sampler.triggerAttackRelease(note, duration);
+            Tone.context.resume().then(() => {
+                const now = Tone.now();
+                sampler.triggerAttack(note, now, 0.5);
+                sampler.triggerRelease(note, now + Tone.Time(duration).toSeconds());
+            });
         }
     };
 
     const playChord = (chord) => {
         if (sampler) {
-            sampler.triggerAttackRelease(chord, '2n');
+            Tone.context.resume().then(() => {
+                const now = Tone.now();
+                chord.forEach(note => {
+                    sampler.triggerAttack(note, now, 0.5);
+                    sampler.triggerRelease(note, now + 2);
+                });
+            });
         }
     };
 
